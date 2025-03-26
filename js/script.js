2ebf4f89061bffda6795c3c46110b86f7bc27a28
ilariamaffiola <maffiolailaria1@gmail.com>
2025-03-26 16:05:24 +0100

    [UPD]Refactor form structure in index.html and add DOM element retrieval in script.js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//RECUPERO TUTTI GLI ELEMENTI DEL DOM E DICHIARO LE VARIABILI
+//Recupero i campi form e della form stessa
+const buttonGenerate = document.getElementById('button-generate');
+const buttonDelete = document.getElementById('button-delete');
+const nameField = document.getElementById('nome-field');
+const surnameField = document.getElementById('surname-field');
+const kilometerField = document.getElementById('kilometer-field');
+const ageField = document.getElementById('age-field');

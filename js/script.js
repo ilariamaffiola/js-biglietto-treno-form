24ecfc7ad0d518a17e43247d0353bd200ea9b86a
ilariamaffiola <maffiolailaria1@gmail.com>
2025-03-26 16:48:32 +0100

    [UPD]Update passenger details display and enhance ticket information retrieval in script.js

--- js/script.js
+++ js/script.js
@@ -6,3 +6,9 @@ const nameField = document.getElementById('nome-field');
 const surnameField = document.getElementById('surname-field');
 const kilometerField = document.getElementById('kilometer-field');
 const ageField = document.getElementById('age-field');
+//recupero i segnaposti e il tipo di biglietto che dovrebbe servirmi dopo
+const nome = document.getElementById('nome');
+const surname = document.getElementById('surname');
+const typeOfTicket = document.getElementById('type-ticket');
+const costo = document.getElementById('balance');
+
